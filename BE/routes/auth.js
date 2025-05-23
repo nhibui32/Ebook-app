7ca50767d84220afe5b0138e54a89de5291a9809
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-05-22 21:44:59 -0500

    add BE

--- BE/routes/auth.js
+++ BE/routes/auth.js
@@ -11,14 +11,15 @@ const router = express.Router();
 
 // Register
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, email, password } = req.body;
+  console.log('BODY:', req.body);
 
   try {
     const existingUser = await User.findOne({ username });
     if (existingUser) return res.status(400).json({ message: 'Username already taken' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword });
+    const user = new User({ username, email, password: hashedPassword });
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully' });
@@ -51,6 +52,7 @@ router.post('/login', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: 'Server error during login' });
   }
+
 });
 
 // Protected route
--- BE/server.js
+++ BE/server.js
@@ -15,12 +15,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 // DB + Server
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT, () =>
-    console.log(`Server running on http://localhost:${process.env.PORT}`)
-  );
-}).catch(err => console.error('DB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error('DB connection error:', err));
